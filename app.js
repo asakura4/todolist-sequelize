3b1e2afc406066961d9bf4d5a235af957beae1f7
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-01-22 15:14:40 -0800

    add login

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const methodOverride = require('method-override')
+if(process.env.NODE_ENV !== 'prod'){
+    require('dotenv').config()
+}
+
+const usePassport = require('./config/passport')
+const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
 const db = require('./models')
@@ -14,6 +21,17 @@ app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+
+usePassport(app)
+
+
+
 
 app.get('/', (req, res) => {
     return Todo.findAll({
@@ -36,9 +54,10 @@ app.get('/users/login', (req,res)=>{
     res.render('login')
 })
 
-app.post('/users/login', (req,res)=>{
-    res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req,res)=>{
     res.render('register')
@@ -46,8 +65,32 @@ app.get('/users/register', (req,res)=>{
 
 app.post('/users/register', (req,res)=>{
     const {name, email, password, confirmPassword } = req.body
-    User.create({name, email, password})
-        .then(user => res.redirect('/'))
+    const errors = []
+    if (!email || !password || !confirmPassword) {
+        errors.push({ message: 'some column(s) is(are) missing.' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: 'Password does not match.' })
+    }
+    User.findOne( {where: { email }})
+    .then(user => {
+        if(user){
+            // errors.push({message: '這個 Email 已經註冊過了。' });
+            console.log('User already exists')
+            return res.render('register', { errors, name, email, password, confirmPassword });
+        }
+        
+        return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(password, salt))
+            .then(hash => User.create({
+                name,
+                email, 
+                password: hash
+            }))
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err));
+    });
 })
 
 app.post('/users/logout', (req,res)=>{
